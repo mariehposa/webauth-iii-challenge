5b801cec440670fa3795586002b4199ecdb13cfc
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-13 13:59:01 +0000

    create port for server

---
+++ user/userRouter.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const db = require('./userModel')
+const router = express.Router();
+
+router.get('/', (req, res) => {
+
+})
+
+router.post('/register', (req, res) => {
+    
+})
+
+router.post('/login', (req, res) => {
+    
+})
+
+module.exports = router;
