ad83b02c0d90a093c36316e67dfa6cf974249044
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-13 14:22:13 +0000

    create endpoint to login

--- user/userRouter.js
+++ user/userRouter.js
@@ -3,12 +3,20 @@ const bcrypt = require('bcrypt');
 const db = require('./userModel')
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
-})
-
 router.post('/register', (req, res) => {
-    
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10)
+    user.password = hash;
+
+    db.addUser(user)
+    .then(user => {
+        res.status(201).json(user)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'Couldnt register user' + err.message
+        })
+    })
 })
 
 router.post('/login', (req, res) => {
